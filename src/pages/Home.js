0f74ab29a52ea225614b863c142d06bfc6ac6e95
Yersultan <ersultansansyzbaj@gmail.com>
2023-05-19 20:57:01 +0600

    hero banner

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,16 @@
+import { Search } from "@mui/icons-material";
+import { Box } from "@mui/material";
 import React from "react";
+import { HeroBanner } from "../components/HeroBanner";
+import { SearchExercises } from "../components/SearchExercises";
+import { Exercises } from "../components/Exercises";
 
 export const Home = () => {
-  return <div>Home</div>;
+  return (
+    <Box>
+      <HeroBanner />
+      <SearchExercises />
+      <Exercises />
+    </Box>
+  );
 };
-  
